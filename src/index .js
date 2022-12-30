78896af1cc0fa65f6e42e5bd6d457779cdeb4e96
Mark Ramos <ramosmarkjoshua@gmail.com>
2022-12-30 18:53:06 +0800

    Added sidebar hide logic

--- src/index .js
+++ src/index .js
@@ -1,15 +1,23 @@
 //Adding of project
-const addProject = document.querySelector('#addProject')
-const projectForm = document.querySelector('#projectForm')
+const addProject = document.querySelector('#addProject');
+const projectForm = document.querySelector('#projectForm');
 
-addProject.addEventListener('click', function () {
-  projectForm.classList.remove('hidden')
-})
+addProject.addEventListener('click', () => projectForm.classList.remove('hidden'));
 
 //Adding of task
-const addList = document.querySelector('#addList')
-const listForm = document.querySelector('#listForm')
+const addList = document.querySelector('#addList');
+const listForm = document.querySelector('#listForm');
 
-addList.addEventListener('click', function () {
-  listForm.classList.remove('hidden')
-})
+addList.addEventListener('click', () => listForm.classList.remove('hidden'));
+
+//Dark and Light Mode
+const checkbox = document.querySelector('#checkbox');
+
+checkbox.addEventListener('change', () => document.body.classList.toggle('light'));
+
+//Hide menu
+const hiddenMenu = document.querySelector('.hiddenMenu');
+hiddenMenu.addEventListener('click', () => {
+  const leftPanel = document.querySelector('.leftPanel');
+  leftPanel.classList.toggle('hidden');
+});
